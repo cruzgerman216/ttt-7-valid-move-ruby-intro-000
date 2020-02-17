944694b575c6cb82859aabb80c5a41e1ea50e827
German Cruz <cruzgerman216@yahoo.com>
2020-02-17 23:35:22 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,6 +3,6 @@ def valid_move(index)
   if index == 0 || index == 1 || index == 2 || index == 3 || index == 4 || index == 5 || index == 6 || index == 7 || index == 8
 
   end
-end 
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
