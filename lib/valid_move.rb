36617f88cec63fc77bcab81c9d81f722bc3c0f58
German Cruz <cruzgerman216@yahoo.com>
2020-02-17 23:37:57 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -9,6 +9,12 @@ end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def valid_move?(board, index)
-
-
+  if valid_movie(index)
+    if board[index] == " " || board[index] == "" || board[index] == nil
+      true
+    else
+      false
+  else
+    false
+  end 
 end
