6c57a57671ac2571d021ae19ac95659cc1a54c2d
German Cruz <cruzgerman216@yahoo.com>
2020-02-17 23:35:37 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,9 +1,9 @@
 # code your #valid_move? method here
 def valid_move(index)
   if index == 0 || index == 1 || index == 2 || index == 3 || index == 4 || index == 5 || index == 6 || index == 7 || index == 8
-true
-else
-  false
+    true
+  else
+    false
   end
 end
 
